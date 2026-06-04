50cb8e2eb985dc040ad88bbb56cc424fb2150d3a
Alexander Saballos <59106053+alexsaballos@users.noreply.github.com>
2026-06-03 22:44:18 -0600

    Schema.org prop fixing on occupational_experience.ts entity

--- src/config/entities/occupational_experience.ts
+++ src/config/entities/occupational_experience.ts
@@ -3,9 +3,5 @@ import { SYSTEM } from "@config/links";
 export const SCHEMAORG_OCCUPATIONALEXP = {
     "@type": "OccupationalExperienceRequirements",
     "@id": `${SYSTEM.domain}#experience-requirement`,
-    "monthsOfExperience": 12,
-        "qualifications": {
-        "@type": "EducationalOccupationalCredential",
-        "name": "Engineer Intern (EI/EiT) License"
-    }
+    "monthsOfExperience": 12
 } as const;
