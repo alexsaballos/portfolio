b6ecaa9b624f5d655c4b966a68a173786bdea66b
Alexander Saballos <59106053+alexsaballos@users.noreply.github.com>
2026-06-04 02:36:44 -0600

    Fixed AOS v Vanity Redirection issue & Updated deplo.yml

--- src/scripts/countdown_vanity.ts
+++ src/scripts/countdown_vanity.ts
@@ -3,11 +3,15 @@ let seconds: number = 3;
 const url = document.getElementById("countdownVanityContainer")!.dataset.url as string;
 if (!url) throw new Error("Missing redirect URL for countdown vanity.");
 
-const interval = setInterval(() => {
-    seconds--;
-    document.getElementById("countdownVanitySpan")!.textContent = `${seconds}`;
-    if (seconds <= 0) {
-        clearInterval(interval);
-        window.location.href = url;
-    }
-}, 1000);
+// Wait 2s for AOS to settle & start 1s counting interval
+setTimeout(() => {
+    const interval = setInterval(() => {
+        seconds--;
+        document.getElementById("countdownVanitySpan")!.textContent = `${seconds}`;
+        
+        if (seconds <= 0) {
+            clearInterval(interval);
+            window.location.href = url;
+        }
+    }, 1000);
+}, 2000);
