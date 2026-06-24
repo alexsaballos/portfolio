71423a317a7e900389b82c419687bf60be5aead0
Alexander Saballos <59106053+alexsaballos@users.noreply.github.com>
2026-06-23 23:20:03 -0600

    Project's cover correction

--- src/scripts/pages/projects.ts
+++ src/scripts/pages/projects.ts
@@ -1,35 +1,64 @@
+// Element selections: search bar, discipline select, status select, organization select, featured check, clear button, and project cards
 const searchInput = document.getElementById("project-search") as HTMLInputElement;
 const disciplineSelect = document.getElementById("project-discipline") as HTMLSelectElement;
 const statusSelect = document.getElementById("project-status") as HTMLSelectElement;
 const organizationSelect = document.getElementById("project-organization") as HTMLSelectElement;
 const featuredCheck = document.getElementById("project-featured") as HTMLInputElement;
 const clearButton = document.getElementById("project-clear") as HTMLButtonElement;
-const projectCards = document.querySelectorAll<HTMLInputElement>(".project-card");
+const projectCards = document.querySelectorAll<HTMLElement>(".project-card");
+const electricalHero = document.getElementById("electrical-hero") as HTMLElement;
+const softwareHero = document.getElementById("software-hero") as HTMLElement;
 
 if (!searchInput || !disciplineSelect || !statusSelect || !organizationSelect || !featuredCheck || !clearButton)
     throw new Error("[PROJECTS] Missing elements on the DOM");
 
-searchInput.addEventListener("input", () => {
+// Apply selected filters in real time
+const applyFilters = () => {
+    // Fetch input in real time
     const q = searchInput.value.toLowerCase();
-    console.log(q);
-    projectCards.forEach(card => {
-        const project_title = card.dataset.project_title!;
-        const project_discipline = card.dataset.project_discipline!;
-        const project_status = card.dataset.project_status!;
-        const project_stack = card.dataset.project_stack!;
-        const project_organization = card.dataset.project_organization!;
-        const project_featured = card.dataset.project_featured!;
+    const input_discipline = disciplineSelect.value;
+    const input_status = statusSelect.value;
+    const input_organization = organizationSelect.value;
+    const input_featured = featuredCheck.checked;
 
-        card.classList.toggle("hidden", project_title.includes(q));
+    console.log(`${q} - ${input_discipline} - ${input_status} - ${input_organization} - ${input_featured}`);
+    projectCards.forEach(card => {
+        // Fetch project data in real time
+        const card_dataset = card.dataset;
+        const project_title = (card_dataset.project_title ?? "").toLowerCase();
+        const project_discipline = card_dataset.project_discipline ?? "";
+        const project_status = card_dataset.project_status ?? "";
+        const project_stack = card_dataset.project_stack ?? "";
+        const project_organization = card_dataset.project_organization ?? "";
+        const project_featured = card_dataset.project_featured ?? "";
+
+        // Comparison
+        const matchesSearch = (q === "") || project_title.includes(q);
+        const matchesDiscipline = (input_discipline === "all") || (project_discipline === input_discipline);
+        const matchesStatus = (input_status === "all") || (project_status === input_status);
+        const matchesOrganization = (input_organization === "all") || (project_organization === input_organization);
+        const matchesFeatured = !input_featured || project_featured;
+
+        // Visibility calculation & toggling
+        const visible = matchesSearch && matchesDiscipline && matchesStatus && matchesOrganization && matchesFeatured;
+        card.classList.toggle("hidden", !visible);
     })
-})
+};
+
+// Add event listener for all inputs
+searchInput.addEventListener("input", applyFilters);
+disciplineSelect.addEventListener("change", applyFilters);
+statusSelect.addEventListener("change", applyFilters);
+organizationSelect.addEventListener("change", applyFilters);
+featuredCheck.addEventListener("change", applyFilters);
 
 // Returns to default values for the searcher
 clearButton.addEventListener("click", () => {
     searchInput.value = "";
     disciplineSelect.value = statusSelect.value = organizationSelect.value = "all";
     featuredCheck.checked = false;
-    projectCards.forEach(card => {
-        card.classList.toggle("hidden", false);
-    })
+    applyFilters();
 });
+
+// Ensure consistent Initial State
+applyFilters();
